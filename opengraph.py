f4a592684dc9d5aa481bec2fca6192db69adf29f
Flavien Pictet <flavien@pictet.net>
2023-07-18 12:19:40 +0200

    Implemented fetch_metadata using https://github.com/lewagon/opengraph API

--- opengraph.py
+++ opengraph.py
@@ -9,14 +9,17 @@ def fetch_metadata(url):
     """
     Returns a dictionary of OpenGraph metadata found in the HTML of the given URL.
     """
-    try:
-        response = requests.get(url)
-        return response.json()
-    except requests.exceptions.RequestException:
-        return {}
+
+    URI = "https://opengraph.lewagon.com/"
+    params = {"url": url}
+    response = requests.get(URI, params = params)
+
+    if response.status_code == 200:
+        return response.json()['data']
+
+    return {}
 
 
-# To manually test, please uncomment the following lines and run `python opengraph.py`:
-# import pprint
-# pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(fetch_metadata("https://www.github.com"))
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(fetch_metadata("https://www.github.com"))
